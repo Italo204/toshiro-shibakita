e7f1f01e82d21c0c3179075c50af8f2bd208b8fd
Italo204 <italoosantos32@gmail.com>
2024-10-20 16:25:35 -0300

    Desafio DIO

--- config/banco/banco.sql
+++ config/banco/banco.sql
@@ -1,8 +1,9 @@
-CREATE TABLE dados (
+CREATE TABLE IF NOT EXISTS dados (
     AlunoID int,
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
-    Host varchar(50)
+    Host varchar(50),
+    PRIMARY KEY (AlunoID)
 );
--- src/index.php
+++ src/index.php
@@ -4,7 +4,6 @@
 <title>Exemplo PHP</title>
 </head>
 <body>
-
 <?php
 ini_set("display_errors", 1);
 header('Content-Type: text/html; charset=iso-8859-1');
@@ -13,10 +12,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "db:3306";
 $username = "root";
 $password = "Senha123";
-$database = "meubanco";
+$database = "banco";
 
 // Criar conexão
 
